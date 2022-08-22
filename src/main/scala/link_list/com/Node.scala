57553e6c0627e4b84b38641ad3b7125f77443737
19522541 <19522541@gm.uit.edu.vn>
2022-08-22 12:16:40 +0700

    add append method of Node class

--- src/main/scala/link_list/com/Node.scala
+++ src/main/scala/link_list/com/Node.scala
@@ -1,9 +1,26 @@
 package link_list.com
 
+import link_list.com.Node.isCircle
+
+import scala.collection.mutable.RedBlackTree
+
 class Node(private var next :Option[Node]=None) {
   def setNext(nextNote:Option[Node]): Node ={
     this.next= nextNote
     next.get
+  }
+  def append(tailNode:Option[Node]):Option[Node]={
+//    if(Node.isCircle(Option(this))){
+//      return None
+//    }
+    var currentNode= this
+
+      while( currentNode.next.isDefined )
+        {
+          currentNode= currentNode.next.get
+        }
+          currentNode.next=tailNode
+    return tailNode
   }
   def nextNode(): Option[Node] ={
      next
@@ -18,10 +35,15 @@ object Node{
     } else{
       var slower =head
       var faster = slower.get.nextNode()
-      while (slower!=None && faster!=slower && faster!=None ){
+      while (slower.isDefined && faster!=slower && faster.isDefined ){
 
         slower = slower.get.nextNode()
-        faster = faster.get.nextNode().get.nextNode()
+        if( faster.get.nextNode().isDefined){
+          faster = faster.get.nextNode().get.nextNode()
+        }else{
+          return false
+        }
+
       }
       if(faster==slower) {
         return true
--- src/test/scala/NodeTest.scala
+++ src/test/scala/NodeTest.scala
@@ -4,27 +4,29 @@ import org.scalatest.funsuite.AnyFunSuite
 class NodeTest extends AnyFunSuite{
  test("Node.isCirle When linked_list is not circle"){
    val head= new Node()
-   val  t1=  head.setNext(Some(head))
-   val t2=  t1.setNext(Some(new Node()))
-   val t3=  t2.setNext(Some(new Node()))
+   head.append(Option( new Node()))
+   head.append(Option( new Node()))
+   head.append(Option( new Node()))
+
    assert(Node.isCircle(Option(head))===false)
  }
   test("Node.isCirle When linked_list is circle"){
     val head= new Node()
-    val  t1=  head.setNext(Some(head))
-    val t2=  t1.setNext(Some(new Node()))
-    val t3=  t2.setNext(Some(new Node()))
-    val t4=  t3.setNext(Some(new Node()))
-    t3.setNext(Some(t1))
+    head.append(Option( new Node()))
+   val t= head.append(Option( new Node()))
+    head.append(Option( new Node()))
+    head.append(Option( new Node()))
+    head.append(t)
+
     assert(Node.isCircle(Option(head))===true)
   }
   test("Node.isCirle When linked_list is circle in the head of list"){
     val head= new Node()
-    val  t1=  head.setNext(Some(head))
-    val t2=  t1.setNext(Some(new Node()))
-    val t3=  t2.setNext(Some(new Node()))
-    val t4=  t3.setNext(Some(new Node()))
-    t3.setNext(Some(head))
+    head.append(Option( new Node()))
+    head.append(Option( new Node()))
+    head.append(Option( new Node()))
+    head.append(Option(head))
+
     assert(Node.isCircle(Option(head))===true)
   }
   test(("Node.isCirle when linked-list has only one node")){
