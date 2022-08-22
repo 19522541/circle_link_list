59d0198492d8caef38f77ebf2d32b46543c76a0b
19522541 <19522541@gm.uit.edu.vn>
2022-08-22 13:22:08 +0700

    renaming variable

--- src/main/scala/link_list/com/Node.scala
+++ src/main/scala/link_list/com/Node.scala
@@ -1,18 +1,14 @@
 package link_list.com
 
-import link_list.com.Node.isCircle
-
-import scala.collection.mutable.RedBlackTree
-
 class Node(private var next :Option[Node]=None) {
   def setNext(nextNote:Option[Node]): Node ={
     this.next= nextNote
     next.get
   }
   def append(tailNode:Option[Node]):Option[Node]={
-//    if(Node.isCircle(Option(this))){
-//      return None
-//    }
+    if(Node.isCircle(Option(this))){
+      return None
+    }
     var currentNode= this
 
       while( currentNode.next.isDefined )
@@ -20,7 +16,7 @@ class Node(private var next :Option[Node]=None) {
           currentNode= currentNode.next.get
         }
           currentNode.next=tailNode
-    return tailNode
+    tailNode
   }
   def nextNode(): Option[Node] ={
      next
@@ -28,28 +24,25 @@ class Node(private var next :Option[Node]=None) {
 }
 object Node{
   def isCircle(head:Option[Node]): Boolean ={
-
-    if (head==None||head.get.next==None){
-
+    if (head.isEmpty||head.get.next.isEmpty){
       return  false
     } else{
-      var slower =head
-      var faster = slower.get.nextNode()
-      while (slower.isDefined && faster!=slower && faster.isDefined ){
-
-        slower = slower.get.nextNode()
-        if( faster.get.nextNode().isDefined){
-          faster = faster.get.nextNode().get.nextNode()
+      var slowerIncreasingNode =head
+      var fasterIncreasingNode = slowerIncreasingNode.get.nextNode()
+      while (slowerIncreasingNode.isDefined && fasterIncreasingNode!=slowerIncreasingNode && fasterIncreasingNode.isDefined ){
+        slowerIncreasingNode = slowerIncreasingNode.get.nextNode()
+        if( fasterIncreasingNode.get.nextNode().isDefined){
+          fasterIncreasingNode = fasterIncreasingNode.get.nextNode().get.nextNode()
         }else{
           return false
         }
 
       }
-      if(faster==slower) {
+      if(fasterIncreasingNode==slowerIncreasingNode) {
         return true
       }
     }
 
-    return  false
+    false
   }
 }
